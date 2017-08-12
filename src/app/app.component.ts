0b5839427ef09b2834b08213eaea81a172ec36f8
Misha3739 <mihail.udot@yandex.ru>
2017-08-12 16:57:16 +0300

    Complete task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Form, NgForm} from '@angular/forms';
+
+class User
+{
+ email: string;
+ password: string;
+ option: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+
+  submitted = false;
+
   title = 'app';
+
+  options = ['basic', 'advanced', 'prone'];
+
+  defaultOption= 'advanced';
+
+  passwordValid = false;
+  passwordConfirmValid = false;
+
+  password: string;
+  confirmPassword: string;
+
+  user: User;
+
+  constructor()
+  {
+    this.user = new User();
+  }
+
+  onLog()
+  {
+    console.log(this.signupForm);
+  }
+
+  onSubmit(form: NgForm)
+  {
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.password = '';
+    this.user.option = this.signupForm.value.option;
+    this.submitted = true;
+  }
+
+
+  onPasswordChange()
+  {
+    this.passwordValid  =  this.password.length >= 7;
+  }
+
+  onConfirmPasswordChange()
+  {
+    this.passwordConfirmValid =  this.password === this.confirmPassword;
+  }
 }
